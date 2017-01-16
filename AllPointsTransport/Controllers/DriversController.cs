85ee730704d88e97f134b8f227bf9c97f46bf3a3
Jason Freer <jsfreer@gmail.com>
2017-01-16 13:37:57 -0600

    Initial Load

---
+++ AllPointsTransport/Controllers/DriversController.cs
@@ -0,0 +1,128 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AllPointsTransport.Models;
+
+namespace AllPointsTransport.Controllers
+{
+    public class DriversController : Controller
+    {
+        private AllPointsTransportEntities db = new AllPointsTransportEntities();
+
+        // GET: Drivers
+        public ActionResult Index()
+        {
+            return View(db.Drivers.ToList());
+        }
+
+        // GET: Drivers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Driver driver = db.Drivers.Find(id);
+            if (driver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driver);
+        }
+
+        // GET: Drivers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Drivers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DriverID,DriverNo,Active,DriverType,EmployeePayPct,FirstName,LastName,FullName,NickName,Address1,Address2,City,State,Zip,HomePhone,MobilePhone,Email,TextMessage,EmergencyContact,EmergencyPhone,SSNum,DOB,CDLNum,CDLExpiration,MedicalExpiration,TWICExpiration,AnnualReview,HireDate,ResignDate,ResignReason,Rehire,FuelCardNum,Notes,TruckNum,Color,DateCreated,CreatedBy,LastUpdated,UpdatedBy")] Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Drivers.Add(driver);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(driver);
+        }
+
+        // GET: Drivers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Driver driver = db.Drivers.Find(id);
+            if (driver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driver);
+        }
+
+        // POST: Drivers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DriverID,DriverNo,Active,DriverType,EmployeePayPct,FirstName,LastName,FullName,NickName,Address1,Address2,City,State,Zip,HomePhone,MobilePhone,Email,TextMessage,EmergencyContact,EmergencyPhone,SSNum,DOB,CDLNum,CDLExpiration,MedicalExpiration,TWICExpiration,AnnualReview,HireDate,ResignDate,ResignReason,Rehire,FuelCardNum,Notes,TruckNum,Color,DateCreated,CreatedBy,LastUpdated,UpdatedBy")] Driver driver)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(driver).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(driver);
+        }
+
+        // GET: Drivers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Driver driver = db.Drivers.Find(id);
+            if (driver == null)
+            {
+                return HttpNotFound();
+            }
+            return View(driver);
+        }
+
+        // POST: Drivers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Driver driver = db.Drivers.Find(id);
+            db.Drivers.Remove(driver);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
